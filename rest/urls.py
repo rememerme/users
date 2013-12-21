972c1fd7f1067adc2bebec4583bd4eca67c2a7ed
Jake Gregg <greggjs@miamioh.edu>
2013-12-20 21:34:19 -0500

    Changed something before test

--- rest/urls.py
+++ rest/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, include, url
 from rest import views
 
 urlpatterns = patterns('',
-    url(r'^/test$', views.UsersListView.as_view()),
+    url(r'^', views.UsersListView.as_view()),
     url(r'^$', views.UsersListView.as_view()),
 )
