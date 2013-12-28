17cda6fbd891b7066c2c2e6e018f43254632262b
Andrew Oberlin <andyoberlin@gmail.com>
2013-12-28 17:58:46 -0500

    Fixed utility for getting the limit

--- users/util.py
+++ users/util.py
@@ -19,12 +19,12 @@ import hashlib
     @param request: The request being made to the server
 '''
 def getLimit(request):
-    limit = settings.REST_FRAMEWORK.PAGINATE_BY
+    limit = settings.REST_FRAMEWORK['PAGINATE_BY']
     
     # gets the limit of the request and defaults to the maximum if the limit passed is too big
     # also if no limit is sent then the limit in the settings is used
     if 'limit' in request.QUERY_PARAMS:
-        maxLimit = settings.REST_FRAMEWORK.MAX_PAGINATE_BY
+        maxLimit = settings.REST_FRAMEWORK['MAX_PAGINATE_BY']
         limit = maxLimit if request.QUERY_PARAMS['limit'] > maxLimit else request.QUERY_PARAMS['limit']
     
     return limit
