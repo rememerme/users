26156ab4efc1e105ee4e01892619c359d5e78d17
Jake Gregg <greggjs@miamioh.edu>
2014-01-16 15:00:43 -0500

    made users namespaced in URL to /users/...

--- config/urls.py
+++ config/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'NomNomUsers.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^rest/v1/', include('rememerme.users.rest.urls')),
+    url(r'^rest/v1/users', include('rememerme.users.rest.urls')),
 )
