da70a17018d2dba6a3771018178f9089b87bd27a
Andrew Oberlin <andyoberlin@gmail.com>
2014-01-18 04:27:07 -0500

    Fixed a view

--- rememerme/users/rest/views.py
+++ rememerme/users/rest/views.py
@@ -1,13 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-from rememerme.users.serializers import UserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-import pycassa
-from django.conf import settings
 from rememerme.users.rest.forms import UserGetListForm, UserPostForm, UserPutForm, UserGetSingleForm
 from rememerme.users.rest.exceptions import BadRequestException, NotImplementedException
 from rest_framework.permissions import IsAuthenticated
